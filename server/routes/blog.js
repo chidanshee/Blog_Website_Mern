acd1b0225aff828b1b271964905b89a2aa66011a
zalabhavy <zalabhavy2004@gmail.com>
2025-06-04 18:22:48 +0530

    added authmiddleware and give proper route

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -2,6 +2,7 @@ import express from "express";
 import AuthController from "../controllers/authcontroller.js";
 import BlogController from "../controllers/blogcontroller.js";
 import CategoryController from "../controllers/catagorycontroller.js";
+import checkIsUserAuthenticated from "../middlewares/authMiddleWare.js";
 
 const router = express.Router();
 
@@ -10,11 +11,11 @@ router.post("/user/register", AuthController.userRegistration);
 router.post("/user/login", AuthController.userLogin);
 
 // Blog Routes
-router.get("/get/allblogs",BlogController.getAllBlogs);
-router.post("/add/blog", upload.single("thumbnail"),BlogController.addNewBlog);
-router.get("/get/blog/:id",BlogController.getSingleBlog);
+router.get("/get/allblogs",checkIsUserAuthenticated,BlogController.getAllBlogs);
+router.post("/add/blog", upload.single("thumbnail"),checkIsUserAuthenticated,BlogController.addNewBlog);
+router.get("/get/blog/:id",checkIsUserAuthenticated,BlogController.getSingleBlog);
 
 // Category Routes
-router.get("/get/catagories",CategoryController.getAllCategories);
-router.post("/add/catagory",CategoryController.addNewCategory);
+router.get("/get/catagories",checkIsUserAuthenticated,CategoryController.getAllCategories);
+router.post("/add/catagory",checkIsUserAuthenticated,CategoryController.addNewCategory);
 export default router;
