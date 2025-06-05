86bbfe42e6b58e9f3b96c27d4d13cef21630edcd
zalabhavy <zalabhavy2004@gmail.com>
2025-06-05 23:08:57 +0530

    multer, CORS, JSON parser, and static file added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import connectToMongo from "./config/db.js";
 import authRouters from "./routes/blog.js";
+import cors from "cors";
 const app = express();
 const PORT = 9000;
 
 connectToMongo();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public")); 
 
 app.get("/", (req, res) => {
   res.send("API IS RUNNING!");
